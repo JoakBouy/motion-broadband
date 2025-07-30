721109b40310e8d8b298f356838e6a82aa93598b
Buoy Gai <109422686+JoakBouy@users.noreply.github.com>
2025-07-30 18:27:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import PageTransition from './components/PageTransition'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
-import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
 import Privacy from './pages/Privacy'
 
@@ -26,7 +25,6 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/services" element={<Services />} />
-              <Route path="/portfolio" element={<Portfolio />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/privacy" element={<Privacy />} />
             </Routes>
